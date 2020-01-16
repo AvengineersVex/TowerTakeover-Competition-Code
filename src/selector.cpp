8b2d0c6b384e1c6d4ab474dbe295988a5581d157
Gamn69 <34080164+Gamn69@users.noreply.github.com>
2020-01-15 16:06:01 -0800

    Got to printing alliance specific selection.

--- src/selector.cpp
+++ src/selector.cpp
@@ -1,11 +1,71 @@
-#include "vex.h";
+/*Notes
+  alliance variable:
+    0 - red
+    1 - blue
+    2 - skills
+*/
+
+#include "vex.h"
 using namespace vex;
 
 extern brain Brain;
 extern brain::lcd Screen;
 
+void pressedCallback();
+void detectAlliance(int xPosIn, int yPosIn);
+void autonSlector();
+
+int progress, alliance;
+
 void startUp() {
-  Screen.setCursor(10,10);
-  Screen.setPenWidth(20);
-  Screen.print("test");
+  progress = 0;
+  Screen.pressed(pressedCallback);
+  Screen.setFont(prop40);
+  Screen.printAt(130, 50, "Alliance Color");
+  Screen.drawRectangle(100, 80, 100, 80, red);
+  Screen.drawRectangle(280, 80, 100, 80, blue);
+  Screen.drawRectangle(0, 180, 480, 50, green);
+  Screen.setFont(prop20);
+  Screen.printAt(210, 210, "Skills");
+}
+
+void pressedCallback() {
+  int xPos = Screen.xPosition();
+  int yPos = Screen.yPosition();
+  if (progress == 0) {
+    detectAlliance(xPos, yPos);
+  }
+}
+
+void detectAlliance(int xPosIn, int yPosIn) {
+  if (100 < xPosIn && xPosIn < 200 && 80 < yPosIn && yPosIn < 160) { // Red Alliance
+    Screen.clearScreen();
+    progress++;
+    alliance = 0;
+  }
+  else if (280 < xPosIn && xPosIn < 380 && 80 < yPosIn && yPosIn < 160) { // Blue Alliance
+    Screen.clearScreen();
+    progress++;
+    alliance = 1;
+  }
+  else if (0 < xPosIn && xPosIn < 480 && 180 < yPosIn && yPosIn < 240) { // Skills 
+    Screen.clearScreen();
+    progress++;
+    alliance = 2;
+  }
+  autonSlector();
+}
+
+void autonSlector() {
+  Screen.setFont(prop40);
+  if (alliance == 0) { // Red Alliance
+    Screen.printAt(110, 50, "Auton Selection");
+    Screen.drawRectangle(100, 80, 100, 80, purple);
+    Screen.drawRectangle(280, 80, 100, 80, yellow);
+  }
+  else if (alliance == 1) { // Blue Alliance
+    Screen.printAt(110, 50, "Auton Selection");
+    Screen.drawRectangle(100, 80, 100, 80, purple);
+    Screen.drawRectangle(280, 80, 100, 80, yellow);
+  }
 }
